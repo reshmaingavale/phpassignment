f440666f3d5e3d6349dfe09b51088dfb22a28b9a
reshmaingavale <reshma.ingavale@gmail.com>
2012-06-22 15:53:22 +0530

    testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,33 @@
-set :application, "indymultiples"
-set :repository,  "git@git.prometheussports.com:indymultiples/indymultiples.git"
+set :application, "demo"
+set :repository,  "github.com/reshmaingavale/phpassignment.git"
+set :deploy_to, "/var/www/cake_test" 
 
+location : "cake_test.weboapps.com"
 set :scm, :git
 
-set :branch, "master"
+set :branch, "demo"
 set :keep_releases, 3
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "/etc/php2/apache"                          # Your HTTP server, Apache/etc
-role :app, "/etc/php2/apache"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+
+role :web, location                          # Your HTTP server, Apache/etc
+role :app, location                          # This may be the same as your `Web` server
+role :db, location, :primary => true # This is where Rails migrations will run
+
+set :user, 'root'
+set :ssh_options, {:forward_agent => true}
+
+after "deploy","deploy:cleanup"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app do
+    run "/etc/init.d/apache2 restart"
+    run "/etc/init.d/apache2 reload"
+    clear_cache
+  end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -27,12 +44,5 @@ role :db,  "your slave db-server here"
 #   end
 # end
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app do
-    run "/etc/init.d/apache2 restart"
-    run "/etc/init.d/apache2 reload"
-    clear_cache
-  end
+
 
